845e4cd81ec4ed24fb8318581b466d3774da06a6
Ilya Astapov <iastapov114@gmail.com>
2023-03-31 00:11:12 +0300

    Added class MyTreeItem
    закрыл задачку на ютреке

--- MyInterface/mainwindow.h
+++ MyInterface/mainwindow.h
@@ -5,10 +5,26 @@
 #include <QTreeWidget>
 #include <QFileSystemModel>
 #include <QDockWidget>
+#include <QAbstractButton>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+//MyTreeItem
+class MyTreeItem : public QTreeWidgetItem
+{
+
+public:
+    int type;
+    MyTreeItem(MyTreeItem *parent = nullptr, int type = 0);
+    MyTreeItem(QTreeWidget *parent = nullptr, int type = 0);
+
+    //: QTreeWidgetItem(parent)  {}
+    void get_type();
+    void selected();
+};
+
 
 class MainWindow : public QMainWindow
 {
@@ -21,11 +37,14 @@ public:
 private slots:
 
     void on_actionauthors_triggered();
-
     void on_actionExit_triggered();
+    void on_pushButton_2_clicked();
+
+    void on_TreeAddedItems_itemClicked(QTreeWidgetItem *item, int column);
 
 private:
 
     Ui::MainWindow *ui;
 };
+
 #endif // MAINWINDOW_H
